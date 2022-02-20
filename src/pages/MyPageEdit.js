2ac581ca8a88fe9ea0f6c7a76e779f29e0c00d70
hwangjaewon <0912078@gmail.com>
2022-02-20 22:13:47 +0900

    career post add

--- src/pages/MyPageEdit.js
+++ src/pages/MyPageEdit.js
@@ -1,6 +1,7 @@
-import React, { useState } from 'react';
-import { getDocs, collection, deleteDoc, doc, addDoc,setDoc} from "firebase/firestore";
+import React, { useEffect, useState } from 'react';
+import { collection, doc, addDoc,onSnapshot,setDoc,query} from "firebase/firestore";
 import {db,auth} from '../firebase-config';
+import {onAuthStateChanged} from 'firebase/auth';
 function MyPageEdit({user}){
     const [company,setCompany] = useState("");
     const [time,setTime] = useState({
@@ -9,31 +10,27 @@ function MyPageEdit({user}){
         timeEndYear:0,
         timeEndMonth:0
     });
-    // if(user.displayName){
-    //     const usersInfoCollectionRef = collection(db,'userInfo');
-    //     const userDocRef = doc(usersInfoCollectionRef,user.uid);
-    //     // const carrerCollectionRef = collection(userDocRef,'career');
-    // }
-   
+    const [users,setUsers] = useState({});
+    const [careerList,setCareerList]= useState([]);
+    onAuthStateChanged(auth,(currentUser)=>{
+        setUsers(currentUser);
+    })
     const addData = async () => {
         try {
-            await setDoc(doc(db,'userInfo',user.uid),{
-                career:{
+            if(users.displayName){
+                const usersInfoCollectionRef = collection(db,'userInfo');
+                const userDocRef = doc(usersInfoCollectionRef,users.uid);
+                const carrerCollectionRef = collection(userDocRef,'career');
+                await addDoc(carrerCollectionRef,{
                     company:{
                         name:company,
                         time:time
                     }
-                }
-            })
-        } catch (e) {
-          console.log(e);
-          console.log(user.uid);
-          console.log({
-            company:{
-                name:company,
-                time:time
+                })
+                console.log("userinfo에 career 정보 올리기 성공!");
             }
-        })
+        } catch (e) {
+            console.log(e);
         }
     };
     const updateTimeData = e => {
@@ -41,20 +38,42 @@ function MyPageEdit({user}){
             ...time,
             [e.target.name]: +e.target.value
         })
+        console.log(time);
     }
+    useEffect(()=>{
+        console.log("useEffect started");
+        if(users.displayName){
+            console.log("if inside");
+            const careerRef = query(collection(doc(collection(db,'userInfo'),users.uid),'career'));
+            onSnapshot(careerRef,(snapshot)=>{
+                setCareerList(snapshot.docs.map((doc)=>({
+                    ...doc.data(),id:doc.id
+                })));
+            })
+        }
+    },[users])
     // const createPost = async () => {
     //     await addDoc(postsCollectionRef, {title,postText,author:{name:auth.currentUser.displayName,id:auth.currentUser.uid}});
     //     navigate("/");
     // }
+    console.log(careerList);
     return(
         <>
-            <div><h1>{user.displayName}</h1></div>
+            {user&&<div><h1>{user.displayName}</h1></div>}
             <input className="event" placeholder="회사 이름을 입력하세요" onChange={(event)=>{setCompany(event.target.value)}}></input>
             <input className='timeStartYear' name='timeStartYear' placeholder='시작한 연도' type='number' min='1900' max='2100' onChange={updateTimeData} ></input>
             <input className='timeStartMonth' name='timeStartMonth' placeholder='시작한 달' type='number' min='1' max='12' onChange={updateTimeData} ></input>
             <input className='timeEndYear' name='timeEndYear' placeholder='끝난 연도' type='number' min='1900' max='2100' onChange={updateTimeData} ></input>
             <input className='timeEndMonth' name='timeEndMonth' placeholder='끝난 달' type='number' min='1' max='12' onChange={updateTimeData} ></input>
             <button onClick = {addData} >Button Post</button>
+            {careerList && careerList.map((post)=>{
+                return(
+                <div>
+                    {post.company.name}
+                    {post.company.time.timeEndYear}
+                </div>)
+            })}
+
         </>
     )
 }
