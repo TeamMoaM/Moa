8eb4f8a8f4ac29d15d45f7054cf531f3ac665708
hwangjaewon <0912078@gmail.com>
2022-03-01 18:42:47 +0900

    popHeader add

--- src/pages/Post.js
+++ src/pages/Post.js
@@ -109,22 +109,16 @@ function Post({isAuth,user}) {
                 </list>
             }
             <div ref={popHeader}className="popHeader">
-                <div className='postWrap'>
+                <div className='postWrap2'>
                     <div className='scrapReview2'>
-                        <button onClick={()=>scrap()}className='scrap'><div className='scrapFrame'><img className='bookmarkImage' src={Bookmark}/><h3 className='subhead100'>스크랩 하기</h3></div></button>
-                        <Link to={link} className='reviewButton'><h3 className='subhead100'>리뷰 작성하기</h3></Link>
+                        <button onClick={()=>scrap()}className='scrap2'><div className='scrapFrame'><img className='bookmarkImage' src={Bookmark}/><h3 className='subhead100'>스크랩 하기</h3></div></button>
+                        <Link to={link} className='reviewButton2'><h3 className='subhead100'>리뷰 작성하기</h3></Link>
                     </div>
                     <div className="postWrapBox">
-                        <div className='serviceInfo'>
-                            <img className='serviceImgWrap' src={post&&post.imageURL}></img>
+                        <div className='serviceInfo2'>
+                            <img className='serviceImgWrap2' src={post&&post.imageURL}></img>
                             <div className='serviceCon'>
                                 <h4 className='title100'>{post&&post.title}</h4>
-                                <div className='userInfo'>
-                                    <div className='userImg'></div><h4 className='body100'>{post.author&&post.author.name}</h4><h2 className='caption100'>company namy</h2>{/* 회사 이름은 회원가입 페이지 이후 작업 시작 */}
-                                </div>
-                                <div className='serviceEx'>
-                                    <h2 className='body150'>{post&&post.content}</h2>
-                                </div>
                             </div>
                         </div>
                     </div>
@@ -143,7 +137,7 @@ function Post({isAuth,user}) {
             </div>
             <div className='divider'></div>
         </div>
-        <div className="postWrap2">{tabList?<PostServiceIntro/>:<PostReview/>}</div>
+        <div className="postWrap3">{tabList?<PostServiceIntro/>:<PostReview/>}</div>
         </div>
     )
         
