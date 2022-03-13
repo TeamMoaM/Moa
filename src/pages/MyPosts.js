2487770e1117525af7603769247bd1b13edc86fd
woood0207 <woood0207@gmail.com>
2022-03-14 02:49:30 +0900

    MyPost MyReviews 레퍼런스 정리

--- src/pages/MyPosts.js
+++ src/pages/MyPosts.js
@@ -2,8 +2,10 @@ import { getDoc, getDocs, where } from 'firebase/firestore';
 import React, { useState, useEffect } from 'react';
 import { auth,db } from '../firebase-config';
 import {collection,query,orderBy,onSnapshot} from 'firebase/firestore';
+import {useNavigate} from 'react-router-dom';
 function MyPosts(){
     const [posts,setPosts] = useState([]);
+    const navigate = useNavigate();
     useEffect(()=>{
         const postsCollectionRef = collection(db, "posts");
         var q = query(postsCollectionRef, orderBy("time",'desc'),where("author.id","==",auth.currentUser.uid));
@@ -15,13 +17,25 @@ function MyPosts(){
             }
         )
     })
+    const postClick = (id) => {
+        navigate(`/post/${id}`);
+    }
     
     return(
         <div>
             <h1 className='body100'>MyPosts</h1>
             {posts&&posts.map((post)=>{
                 return(
-                    <div>{post.title}</div>
+                <> 
+                    <div onClick={()=>{postClick(post.id)}} className="post">
+                        <img id="myimg" src={post.imageURL}></img>
+                        <div className="post_title">{post.title}</div>
+                        <div className="post_content">{post.content}</div>
+                        <div className="post_commentAndreview">
+                            댓글 {post.commentCount? post.commentCount: 0}개 | 리뷰 {post.reviewCount? post.reviewCount: 0}개
+                        </div>
+                    </div>
+                </>   
                 )
             })}
         </div>
--- src/pages/MyReviews.js
+++ src/pages/MyReviews.js
@@ -1,7 +1,14 @@
 import { collection, doc, getDocs, onSnapshot,query, where } from 'firebase/firestore';
 import React, { useState, useEffect } from 'react';
 import { auth ,db} from '../firebase-config';
+import UserInfo from '../components/UserInfo';
+import TimeCal from '../components/TimeCal';
+import ReviewDetail from '../pages/ReviewDetail';
+import {useNavigate} from 'react-router-dom';
+
 function MyReviews(){
+    const [showReviewNumber,setShowReviewNumber] = useState(0);
+    const navigate = useNavigate();
     const [reviews,setReviews] = useState([]);
     useEffect(()=>{
         var q = query(collection(db,'reviews'),where("reviewPeople","==",auth.currentUser.uid));
@@ -18,7 +25,27 @@ function MyReviews(){
             <h1 className='body100'>MyReviews</h1>
             {reviews&&reviews.map((review)=>{
                 return(
-                    <div>{review.reviewTitle}</div>
+                    <div>
+                        <div onClick={()=>{setShowReviewNumber(review.id);navigate('');}} className="postReviewWrap">
+                            <div className="postReviewBox">
+                                <div className="postReviewTitle">
+                                    <h1 className="subhead100">{review.reviewTitle}</h1>
+                                </div>
+                                {reviews&&<UserInfo uid={review.reviewPeople}/>}
+                                <div className="postReviewContent">
+                                    <h3 className="body150" dangerouslySetInnerHTML={{__html: review.reviewContent}}></h3>
+                                </div>
+                                <div className="postReviewTimeAndLike">
+                                    <TimeCal time={review.time}/>
+                                    <h3 className="body100">|</h3>
+                                    <h3 id="postReviewLike" className="body100">추천 {"0"}개</h3>
+                                </div>
+                            </div>
+                            <div className="postReviewDetailButton">
+                                <h1 className="subhead100">&gt;</h1>
+                            </div>
+                        </div>
+                    </div>
                     // 자세한 레포는 reviews collection 참조 부탁드립니다.
                 )
             })}
