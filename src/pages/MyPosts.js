4b9753ef40d1ceb4010ace3839aa0485964d781d
hwangjaewon <0912078@gmail.com>
2022-03-13 23:55:12 +0900

    button style

--- src/pages/MyPosts.js
+++ src/pages/MyPosts.js
@@ -1,5 +1,11 @@
+import { getDoc, getDocs, where } from 'firebase/firestore';
 import React, { useState, useEffect } from 'react';
+import { auth } from '../firebase-config';
 function MyPosts(){
+    useEffect(()=>{
+        const postsCollectionRef = collection(db, "posts");
+        var q = query(postsCollectionRef, orderBy("time",'desc'),where("author.id","==",auth.currentUser.uid))
+    })
     return(
         <div>
             <h1 className='body100'>MyPosts</h1>
