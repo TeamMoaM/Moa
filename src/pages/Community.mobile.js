4adbe0db7cf20da5d9d1d2c9e7d3ca510ed623be
hyojin <injulme@naver.com>
2022-03-28 23:15:13 +0900

    커뮤니티 작업중1

--- src/pages/Community.mobile.js
+++ src/pages/Community.mobile.js
@@ -1,13 +1,87 @@
+import { useState } from "react";
 import avatarIcon from "../icons/avatar.svg";
+import bronzeMedal from "../img/medals/bronzeMedal.svg";
+import dotsIcon from "../icons/dots.svg";
 function Community() {
+  const [activeIndex, setActiveIndex] = useState(0);
+  const tabList = [
+    { id: 0, title: "전체" },
+    { id: 1, title: "사업" },
+    { id: 2, title: "개발" },
+    { id: 3, title: "기획" },
+    { id: 4, title: "디자인" },
+    { id: 5, title: "기술" },
+  ];
+
+  const tabSelected = (id) => {
+    setActiveIndex(id);
+  };
+
   return (
     <div className="mobile-community">
-      <button className="mobile-community-new_writing">
-        <img src={avatarIcon} className="mobile-avatar" />
-        <h2 className="paragraph200 text-gray-2">
-          회원님의 이야기를 공유해주세요.
-        </h2>
-      </button>
+      <div className="mobile-tab_list">
+        <div className="mobile-tab_list-items">
+          {tabList.map((value, index) => {
+            const active = activeIndex === value.id;
+            return (
+              <div
+                key={index}
+                className={`mobile-tab_list-item ${active && "active"}`}
+                onClick={() => tabSelected(value.id)}
+              >
+                <h3
+                  className={`${
+                    active ? "subhead100 text-primary-1" : "body100"
+                  }`}
+                >
+                  {value.title}
+                </h3>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+      <div className="mobile-community-body">
+        <button className="mobile-community-new_writing">
+          <img src={avatarIcon} className="mobile-avatar-sm mr-8" />
+          <h2 className="paragraph200 text-gray-2">
+            회원님의 이야기를 공유해주세요.
+          </h2>
+        </button>
+        <div className="mobile-community-writing_card">
+          <div className="mobile-user_info">
+            <div className="d-flex align-items-center">
+              <img src={avatarIcon} className="mobile-avatar-md mr-8" />
+              <div className="user-box">
+                <div className="user-title">
+                  <img src={bronzeMedal} className="mobile-medal" />
+                  <h3 className="subhead100 mr-8">유저아이디</h3>
+                  <h3 className="body100 text-gray-1">회사이름</h3>
+                </div>
+                <h2 className="caption100 text-gray-1 mt-3">1시간 전</h2>
+              </div>
+            </div>
+            <div>
+              <img src={dotsIcon} className="mobile-dot-md" />
+            </div>
+          </div>
+          <div className="tag_select">
+            <div className="chip">
+              <h2 className="caption100 text-white">디자인</h2>
+            </div>
+          </div>
+          <div className="mt-16">
+            <h2 className="paragraph200">
+              인간이 어디 소금이라 청춘 이는 부패뿐이다. 석가는 인도하겠다는
+              스며들어 과실이 있는 무엇을 사막이다. 열락의 같은 뛰노는 그들의
+              커다란 속잎나고, 것이다.
+            </h2>
+          </div>
+          <div className="btn_more mt-16">
+            <h1 className="caption100">더보기 &gt;</h1>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
--- src/pages/Header.mobile.js
+++ src/pages/Header.mobile.js
@@ -30,45 +30,21 @@ export default function Header() {
     },
   ];
   return (
-    <>
-      <div className="mobile-header">
-        <div className="mobile-header-title">
-          {headerList.map((value, index) => {
-            if (!value.active) return;
-            if (value.isImg) {
-              return <img src={value.src} alt="MOA 로고" key={index} />;
-            } else {
-              return (
-                <h1 className="subhead100 text-primary-1" key={index}>
-                  {value.title}
-                </h1>
-              );
-            }
-          })}
-        </div>
+    <div className="mobile-header">
+      <div className="mobile-header-title">
+        {headerList.map((value, index) => {
+          if (!value.active) return;
+          if (value.isImg) {
+            return <img src={value.src} alt="MOA 로고" key={index} />;
+          } else {
+            return (
+              <h1 className="subhead100 text-primary-1" key={index}>
+                {value.title}
+              </h1>
+            );
+          }
+        })}
       </div>
-      {location.pathname === "/app/community" && (
-        <div className="mobile-tab_list">
-          <button>
-            <h3 className="body100">전체</h3>
-          </button>
-          <button>
-            <h3 className="body100">사업</h3>
-          </button>
-          <button>
-            <h3 className="body100">개발</h3>
-          </button>
-          <button>
-            <h3 className="body100">기획</h3>
-          </button>
-          <button>
-            <h3 className="body100">디자인</h3>
-          </button>
-          <button>
-            <h3 className="body100">기술</h3>
-          </button>
-        </div>
-      )}
-    </>
+    </div>
   );
 }
